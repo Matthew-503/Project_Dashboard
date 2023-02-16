73af2c5c1c39b67545db6c0c31fe56cd348f3e0d
Matthew Ng <nglingshan30@gmail.com>
2023-02-15 21:06:52 -0700

    Home Page modification

---
+++ client/src/components/charts/TotalRevenue.tsx
@@ -0,0 +1,12 @@
+import ReactApexChart from 'react-apexcharts';
+import { Box, Typography, Stack } from '@pankod/refine-mui';
+import { ArrowCircleUpRounded } from '@mui/icons-material';
+import { TotalRevenueOptions, TotalRevenueSeries } from './chart.config';
+
+const TotalRevenue = () => {
+    return (
+        <div>TotalRevenue</div>
+    )
+}
+
+export default TotalRevenue
