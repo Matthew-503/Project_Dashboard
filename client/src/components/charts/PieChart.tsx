f90c24e7ecc2ce51cdf75d1bd766cde8ea66d5e6
Matthew Ng <nglingshan30@gmail.com>
2023-02-16 00:34:45 -0700

    Minor modification on Pie chart

--- client/src/components/charts/PieChart.tsx
+++ client/src/components/charts/PieChart.tsx
@@ -22,7 +22,7 @@ const PieChart = ({ title, value, series, colors }: PieChartProps) => {
         >
             <Stack direction="column">
                 <Typography fontSize={14} color="#808191">{title}</Typography>
-                <Typography fontSize={24} color="#11142d" fontWeight={700} mt={1}>{value}</Typography>
+                <Typography fontSize={24} fontWeight={700} color="#11142d" mt={1}>{value}</Typography>
             </Stack>
 
             <ReactApexChart
