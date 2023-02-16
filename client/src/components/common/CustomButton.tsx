b3480e3f22ae49898c115c5f154d579bdaa30d5d
Matthew Ng <nglingshan30@gmail.com>
2023-02-16 00:35:35 -0700

    Properties and Custom Button modification

--- client/src/components/common/CustomButton.tsx
+++ client/src/components/common/CustomButton.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Button, fontWeight, minWidth } from '@pankod/refine-mui';
+import { CustomButtonProps } from 'interfaces/common';
 
-const CustomButton = () => {
+const CustomButton = ({ type, fullWidth, icon, title, color, backgroundColor, handleClick }: CustomButtonProps) => {
     return (
-        <div>CustomButton</div>
+        <Button
+            sx={{
+                width: fullWidth ? '100%' : 'fit-content',
+                minWidth: 130,
+                flex: fullWidth ? 1 : 'unset',
+                padding: '10px 15px',
+                fontSize: 16,
+                fontWeight: 600,
+                color,
+                backgroundColor,
+                gap: '10px',
+                textTransform: 'capitalize',
+                '&:hover': {
+                    opacity: 0.9,
+                    backgroundColor
+                }
+            }}
+
+            onClick={handleClick}
+
+        >
+            {icon}
+            {title}
+        </Button>
     )
 }
 
--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Add } from '@mui/icons-material';
+import { useList } from '@pankod/refine-core';
+import { Box, Stack, Typography } from '@pankod/refine-mui';
+import { useNavigate } from '@pankod/refine-react-router-v6';
+import { PropertyCard, CustomButton } from 'components';
 
-const allProperties = () => {
+const AllProperties = () => {
+    const navigate = useNavigate();
     return (
-        <div>allProperties</div>
+        <Box>
+            <Stack direction='row' alignItems='center' justifyContent='space-between'>
+                <Typography fontSize={25} fontWeight={700} color="#11142d">
+                    All Properties
+                </Typography>
+
+                <CustomButton
+                    title='Add Property'
+                    handleClick={() => navigate('/properties/create')}
+                    color='#fcfcfc'
+                    backgroundColor='#475be8'
+                    icon={<Add />}
+                />
+            </Stack>
+        </Box>
     )
 }
 
-export default allProperties
+export default AllProperties
