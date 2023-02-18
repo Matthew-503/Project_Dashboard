68ace7761f174c63980b9663919b097e04d7865c
Matthew Ng <nglingshan30@gmail.com>
2023-02-18 01:15:49 -0700

    Server user.controller create user function

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,33 +1,35 @@
 import User from '../mongodb/models/user.js';
 
-const getAllUsers = async (req, res) => {
-  // try {
-  //   const users = await User.find({}).limit(req.query._end);
+const getAllUsers = async (req, res) => { };
+// try {
+//   const users = await User.find({}).limit(req.query._end);
 
-  //   res.status(200).json(users);
-  // } catch (error) {
-  //   res.status(500).json({ message: error.message })
-  // }
-};
+//   res.status(200).json(users);
+// } catch (error) {
+//   res.status(500).json({ message: error.message })
+// }
 
 const createUser = async (req, res) => {
-  // try {
-  //   const { name, email, avatar } = req.body;
+  try {
+    // retrieve data from frontend
+    const { name, email, avatar } = req.body;
 
-  //   const userExists = await User.findOne({ email });
+    // check if user exist
+    const userExists = await User.findOne({ email });
 
-  //   if(userExists) return res.status(200).json(userExists);
+    if (userExists) return res.stats(200).json(userExists);
 
-  //   const newUser = await User.create({
-  //     name,
-  //     email,
-  //     avatar
-  //   })
+    // user does not exist
+    const newUser = await User.create({
+      name,
+      email,
+      avatar,
+    })
 
-  //   res.status(200).json(newUser);
-  // } catch (error) {
-  //   res.status(500).json({ message: error.message })
-  // }
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const getUserInfoByID = async (req, res) => {
