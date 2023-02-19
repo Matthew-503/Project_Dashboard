cde8d279c696ead65b463439710537ef1933cbe7
Matthew Ng <nglingshan30@gmail.com>
2023-02-19 01:19:12 -0700

    Server routes big fix attempt

--- server/routes/property.routes.js
+++ server/routes/property.routes.js
@@ -1,19 +1,15 @@
 import express from 'express';
 
 import {
-  createProperty,
-  getAllProperties,
-  getPropertyDetail,
-  deleteProperty,
-  updateProperty,
+  createProperty, deleteProperty, getAllProperties, getPropertyDetail, updateProperty
 } from '../controllers/property.controller.js'
 
 const router = express.Router();
 
-router.route('/').post(createProperty);
 router.route('/').get(getAllProperties);
 router.route('/:id').get(getPropertyDetail);
-router.route('/:id').delete(deleteProperty);
+router.route('/').post(createProperty);
 router.route('/:id').patch(updateProperty);
+router.route('/:id').delete(deleteProperty);
 
 export default router;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,15 +1,13 @@
 import express from 'express';
 
-import {
-  createUser,
-  getAllUsers,
-  getUserInfoByID
+import { 
+  createUser, getAllUsers, getUserInfoByID 
 } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.route('/').post(createUser);
 router.route('/').get(getAllUsers);
+router.route('/').post(createUser);
 router.route('/:id').get(getUserInfoByID);
 
 export default router;
