fb6cb6a822879b86060992779afe7ef5c95f3097
Matthew Ng <nglingshan30@gmail.com>
2023-02-23 20:14:31 -0700

    Add files

--- client/src/components/RenderPageOnScreen.js
+++ client/src/components/RenderPageOnScreen.js
@@ -1,5 +1,23 @@
 
 
+    export * from './FileViewer';
+    export { default } from './FileViewer'
+
+
+    import FileViewer from '../FileViewer.js'
 
 
 
+
+
+
+    
+
+
+
+
+
+
+
+
+    FileViewer
